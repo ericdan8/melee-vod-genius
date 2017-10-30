6d27b7f54d6a2a1e76573e4bbc417a9262ee06d9
Eric Dan <eric.dan8@gmail.com>
2017-10-29 22:50:06 -0400

    split non overlapping comments into rows

--- src/components/analysisView/commentTimeline/CommentTimeline.js
+++ src/components/analysisView/commentTimeline/CommentTimeline.js
@@ -9,12 +9,8 @@ export default class CommentTimeline extends React.Component {
     this.width = this.props.width || 640;
     this.duration = this.props.duration;
     this.comments = this.props.comments;
-    this._rows = [];
+    this._rows = [[]];
     this.putCommentsIntoRows();
-  }
-
-  putCommentsIntoRows = () => {
-    this._rows[0] = this.comments;
   }
 
   render = () => (
@@ -23,4 +19,28 @@ export default class CommentTimeline extends React.Component {
     </div>
   )
 
+  putCommentsIntoRows = () => {
+    let rowCandidate = 0;
+
+    this.comments.forEach(comment => {
+      rowCandidate = 0;
+      while (rowCandidate < this._rows.length) {
+        if (this.canBeInRow(this._rows[rowCandidate], comment)) {
+          this._rows[rowCandidate].push(comment);
+          break;
+        }
+        rowCandidate += 1;
+      }
+      // no suitable row found
+      if (rowCandidate === this._rows.length) {
+        this._rows.push([comment]);
+      }
+    }, this);
+  }
+
+  canBeInRow = (row, comment) => {
+    return !row.some(rowComment => this.doCommentsOverlap(rowComment, comment), this);
+  }
+
+  doCommentsOverlap = (c1, c2) => c1.startTime > c2.startTime ? (c2.endTime > c1.startTime) : (c1.endTime > c2.startTime);
 }
